17a89581d8f78b5cd582d311fd20643e456f79bf
Serhii Dorozhan <s.dorozhan@gmail.com>
2022-07-14 16:40:32 +0300

    Added storage module.

---
+++ app/src/main/java/com/dorozhan/catfacts/di/StorageModule.kt
@@ -0,0 +1,37 @@
+package com.dorozhan.catfacts.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.dorozhan.catfacts.data.local.Storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class StorageModule {
+
+    @Provides
+    @Singleton
+    fun provideStorage(prefs: SharedPreferences): Storage {
+        return Storage(prefs)
+    }
+
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+        val mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+        return EncryptedSharedPreferences.create(
+            "app_shared_prefs",
+            mainKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+}
